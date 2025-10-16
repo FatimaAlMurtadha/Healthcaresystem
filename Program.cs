88d08d45466259e65f19e5004f4d90ff83622305
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-16 13:46:40 +0200

    Creat a class to handle the whole system logic with functions

--- Program.cs
+++ Program.cs
@@ -3,12 +3,9 @@ using App;
 
 
 /*
-As a user, I need to be able to log in.
+
 
 As a user, I need to be able to log out.
-
-As a user, I need to be able to request registration as a patient.
-
 As an admin with sufficient permissions, I need to be able to give admins the permission to handle the permission system, in fine granularity.
 
 As an admin with sufficient permissions, I need to be able to assign admins to certain regions.
@@ -51,13 +48,10 @@ As a logged in user, I need to be able to view my schedule.
 */
 
 // En lista på alla registration requests
-List<RequestRegistration> request_registrations = new List<RequestRegistration>();
+
+SystemLogicMenu menu = new SystemLogicMenu();
 IUser? active_user = null;
 bool running = true;
-
-List<IUser> users = new List<IUser>();
-users.Add(new User("Fatima", "123")); // Behövs en User Class 
-
 
 
 while (running)
@@ -66,29 +60,71 @@ while (running)
   {
     System.Console.WriteLine("------------  Health Care System  -------------");
     System.Console.WriteLine("-----------------------------------------------------");
-    System.Console.WriteLine("Log in firsta to open the system");
+    System.Console.WriteLine("Log in As: ");
     System.Console.WriteLine();
-
-    // Loga in för att öpnna systemet
-    System.Console.WriteLine("Username: ");
-    string? username = Console.ReadLine();
-
-    Console.Clear();
-    System.Console.WriteLine("Password: ");
-    string? password = Console.ReadLine();
-    users.Add(new User(username, password));
-    Console.Clear();
-
-    // foreach loop för att kolla om TryLogIns Info är korrekt
-    foreach (IUser user in users)
+    System.Console.WriteLine("1. User.");
+    System.Console.WriteLine("2. Patient.");
+    System.Console.WriteLine("3. Personnel.");
+    System.Console.WriteLine("4. Admin");
+    string? input = Console.ReadLine();
+    switch(input)
     {
-      if (user.TryLogin(username, password))
-      {
-        active_user = user;
-        break;
-      }
-    }
+      case "1": // User
+      if (active_user.IsRole(Role.User))
+        {
+          System.Console.WriteLine("------Welcome User ------");
+          System.Console.WriteLine("Choose one of the following: ");
 
+          System.Console.WriteLine("1. Request registration: ");
+          System.Console.WriteLine("2. log out");
+          System.Console.WriteLine("3. Quit");
+          string? userinput = Console.ReadLine();
+          switch (input)
+          {
+            case "1": // request registration
+              menu.SendRegistrationRequest();
+              break; // slut av request registration
+
+            case "2": // log out
+              active_user = null;
+              break;
+
+            case "3": // quit
+              running = false;
+              break;
+
+            default:
+              System.Console.WriteLine("Invalid input. Press Enter to continue");
+
+              break;
+          }
+        }
+        break;
+      case "2": // Patient
+        if (active_user.IsRole(Role.Patient))
+        {
+
+        } // slut på patient meny
+        break;
+      case "3": // Personnel
+        if (active_user.IsRole(Role.Personnel))
+        {
+
+        } // slut på Personnel meny
+        break;
+      case "4": // Admin
+        if (active_user.IsRole(Role.Main_Admin))
+        {
+
+
+        } // slut på Main Admin meny
+        if (active_user.IsRole(Role.Local_Admin))
+        {
+
+        } // slut på Local Admin meny
+        break;
+
+    }
   }
   // Else satsen Öppnar menyer beroende på Role "Vem som är inloggad"
   else
@@ -97,77 +133,10 @@ while (running)
     System.Console.WriteLine("------------Welcom to Health Care System-------------");
     System.Console.WriteLine("-----------------------------------------------------");
     // Här Kollar vi Role för att öpnna meny beroende på role
-    if (active_user.IsRole(Role.User))
-    {
-      System.Console.WriteLine("------Welcome User ------");
-      System.Console.WriteLine("Choose one of the following: ");
 
-      System.Console.WriteLine("1. Request registration: ");
-      System.Console.WriteLine("2. log out");
-      System.Console.WriteLine("3. Quit");
-      string? input = Console.ReadLine();
-      switch (input)
-      {
-        case "1": // request registration
-          try{ Console.Clear();}catch{}
-          System.Console.WriteLine("Your personal security number: ");
-          string? patientpersonalnumber = Console.ReadLine();
-          System.Console.WriteLine("Your name: ");
-          string? patientname = Console.ReadLine();
-          System.Console.WriteLine("Your email: ");
-          string? patientemail = Console.ReadLine();
-          System.Console.WriteLine("Your password: ");
-          string? patientpassword = Console.ReadLine();
-
-          if (patientpersonalnumber != null && patientpersonalnumber != "" && patientname != null && patientname != "" && patientemail != null && patientemail != "" && patientpassword != null && patientpassword != "")
-          {
-
-            request_registrations.Add(new RequestRegistration(patientpersonalnumber, patientname, patientemail, patientpassword, RegistrationStatus.Pending));
-            System.Console.WriteLine("Your request has been sent susseccfully");
-          }
-          else
-          {
-            System.Console.WriteLine("Invlid input. Nothing of the information can be null or empty");
-            System.Console.WriteLine("Press ENTER to continue......");
-            Console.ReadLine();
-          }
-
-          
-
-          break; // slut av request registration
-
-        case "2": // log out
-          active_user = null;
-          break;
-
-        case "3": // quit
-          running = false;
-          break;
-
-        default:
-          System.Console.WriteLine("Invalid input. Press Enter to continue");
-
-          break;
-      }
-
-    } // slut på user meny
-    if (active_user.IsRole(Role.Main_Admin))
-    {
-
-
-    } // slut på Main Admin meny
-    if (active_user.IsRole(Role.Local_Admin))
-    {
-
-    } // slut på Local Admin meny
-    if (active_user.IsRole(Role.Personnel))
-    {
-
-    } // slut på Personnel meny
-    if (active_user.IsRole(Role.Patient))
-    {
-
-    } // slut på patient meny
+    
+    
+   
 
 
   } // slut på else satsen som öppnar systemet
