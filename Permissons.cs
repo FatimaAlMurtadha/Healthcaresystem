7d698e557d49b7e7885bd7f04ce2279204ee8d9c
OskarUnleashed <oskar.gyllenor@gmail.com>
2025-10-21 10:49:40 +0200

    Från bitflags -> normal

--- Permissons.cs
+++ Permissons.cs
@@ -1,18 +1,17 @@
 namespace App;
 
-[Flags]  //Varje permissions får unikt binärt värde
 public enum Permission
 {
-    None                = 0,  //
-    ManagePermissions = 1 << 0,  //1
-    ManageLocations = 1 << 1,   //2
-    ManagePersonnels = 1 << 2,  //4
-    ManageRegistrations = 1 << 3, //8
-    ManageJournals = 1 << 4, //16
-    ManageAppointments = 1 << 5, //32
-    ManageLocalAdmins = 1 << 6, //64
+    HandlePermissions,
+    AssignRegion,
+    HandleRegistrations,
+    AddLocation,
+    CreatePersonnel,
+    ViewPermissionList,
+    AcceptPatient,
+    DenyPatient,
+    ViewJournals,
+    ApproveAppointments,
+    ModifyAppointments,
+    ViewSchedule
 }
-
-//Så om Jackson har ManageLocations (2), ManagePersonnels (4) och ManageRegistration (8) och ManageAppointments (32)
-//Så blir det binärt 00101110 eller (2 + 4 + 8 + 32 = 46). Eller Jackson:46 
-//
